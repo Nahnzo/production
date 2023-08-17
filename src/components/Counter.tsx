f7ee4a1f2de50a7194ff87f2285ce828231e4558
Dogadaev Alexey <dogadaev142@gmail.com>
2023-08-17 21:09:28 +0300

    scss

---
+++ src/components/Counter.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import "./Counter.scss";
+
+export const Counter = () => {
+  const [count, setCount] = useState(0);
+  const increment = () => {
+    setCount(count + 1);
+  };
+  return (
+    <div>
+      {count}
+      <button onClick={increment}>Increment</button>
+    </div>
+  );
+};
+
+export default Counter;
