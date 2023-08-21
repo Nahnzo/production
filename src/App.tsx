648c0c80af188209b8c04a7948f6d3b29977dfde
Dogadaev Alexey <dogadaev142@gmail.com>
2023-08-21 16:54:43 +0300

    Suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
+import { Route, Routes } from "react-router-dom";
 import Counter from "./components/Counter";
 import "./index.scss";
+
+import { Link } from "react-router-dom";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { Suspense } from "react";
 
 const App = () => {
   return (
     <div className="App">
-      ss
-      <Counter />
+      <Link to={"/"}>Главная</Link>
+      <Link to={"/about"}>Сайте</Link>
+      <Suspense fallback={<div>Load</div>}>
+        <Routes>
+          <Route path={"/about"} element={<AboutPageAsync />} />
+          <Route path={"/"} element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
