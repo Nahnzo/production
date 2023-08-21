5e440e9e6e393e0aeda271f0b8c7fb46a6d2b68b
Dogadaev Alexey <dogadaev142@gmail.com>
2023-08-21 22:09:59 +0300

    toggleTheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import { Route, Routes } from "react-router-dom";
-import Counter from "./components/Counter";
-import "./index.scss";
-
 import { Link } from "react-router-dom";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 import { Suspense } from "react";
+import { useTheme } from "./theme/useTheme";
+import "./styles/index.scss";
 
 const App = () => {
+  const { theme, toggleTheme } = useTheme();
   return (
-    <div className="App">
+    <div className={`app ${theme}`}>
+      <button onClick={() => toggleTheme()}>Toggle</button>
       <Link to={"/"}>Главная</Link>
       <Link to={"/about"}>Сайте</Link>
       <Suspense fallback={<div>Load</div>}>
