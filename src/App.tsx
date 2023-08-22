f6e13416b40cf2f1d821ec58e7ff3328617ac52b
Dogadaev Alexey <dogadaev142@gmail.com>
2023-08-22 16:02:05 +0300

    c

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,12 @@ import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 import { Suspense } from "react";
 import { useTheme } from "./theme/useTheme";
 import "./styles/index.scss";
+import { classNames } from "./helpers/classNames/classNames";
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
   return (
-    <div className={`app ${theme}`}>
+    <div className={classNames("app", {}, [theme])}>
       <button onClick={() => toggleTheme()}>Toggle</button>
       <Link to={"/"}>Главная</Link>
       <Link to={"/about"}>Сайте</Link>
