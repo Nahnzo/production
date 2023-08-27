c8721f798aa70c89a2a45662162a05c58b49f222
Dogadaev Alexey <dogadaev142@gmail.com>
2023-08-27 22:55:13 +0300

    Router structure

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,8 @@
-import { Route, Routes } from "react-router-dom";
 import { Link } from "react-router-dom";
-import { Suspense } from "react";
 import { useTheme } from "app/providers/ThemeProvider";
-import "./styles/index.scss";
 import { classNames } from "shared/lib/classNames/classNames";
-import { AboutPage } from "pages/AboutPage";
-import { MainPage } from "pages/MainPage";
+import { AppRouter } from "./providers/router";
+import "./styles/index.scss";
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
@@ -14,12 +11,7 @@ const App = () => {
       <button onClick={() => toggleTheme()}>Toggle</button>
       <Link to={"/"}>Главная</Link>
       <Link to={"/about"}>Сайте</Link>
-      <Suspense fallback={<div>Load</div>}>
-        <Routes>
-          <Route path={"/about"} element={<AboutPage />} />
-          <Route path={"/"} element={<MainPage />} />
-        </Routes>
-      </Suspense>
+      <AppRouter />
     </div>
   );
 };
