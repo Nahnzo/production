db572100995838565d388295f0f87a1568700e6f
Dogadaev Alexey <dogadaev142@gmail.com>
2023-08-29 16:16:56 +0300

    add swicher

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -3,6 +3,11 @@ import webpack from "webpack";
 import { BuildOptions } from "./types/config";
 
 export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+  const svgLoader = {
+    test: /\.svg$/,
+    use: ["@svgr/webpack"],
+  };
+
   const cssLoaders = {
     test: /\.s[ac]ss$/i,
     use: [
@@ -19,10 +24,19 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     ],
   };
 
+  const fileLoader = {
+    test: /\.(png|jpe?g|gif|woff2|woff)$/i,
+    use: [
+      {
+        loader: "file-loader",
+      },
+    ],
+  };
+
   const typescriptLoader = {
     test: /\.tsx?$/,
     use: "ts-loader",
     exclude: /node_modules/,
   };
-  return [typescriptLoader, cssLoaders];
+  return [typescriptLoader, cssLoaders, fileLoader, svgLoader];
 }
---
+++ src/shared/ui/Button/Button.tsx
@@ -0,0 +1,27 @@
+import { classNames } from "shared/lib/classNames/classNames";
+import { ButtonHTMLAttributes, FC } from "react";
+import styles from "./Button.module.scss";
+
+export enum ThemeButton {
+  CLEAR = "clear",
+}
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  className?: string;
+  theme?: ThemeButton;
+}
+
+const Button: FC<ButtonProps> = (props) => {
+  const { className, theme, children, ...otherProps } = props;
+
+  return (
+    <button
+      className={classNames(styles.ThemeSwitcher, {}, [className, styles[theme]])}
+      {...otherProps}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
