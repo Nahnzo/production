8a13c38a659bf92598a2d76040a25daa0c58706c
Dogadaev Alexey <dogadaev142@gmail.com>
2023-09-27 17:11:18 +0300

    precommit

--- src/feautures/AuthByUsername/ui/LoginModal/LoginModal.tsx
+++ src/feautures/AuthByUsername/ui/LoginModal/LoginModal.tsx
@@ -1,6 +1,6 @@
 import { classNames } from "shared/lib/classNames/classNames";
 import Modal from "shared/ui/Modal/Modal";
-import styles from "./LoginModal.module.scss";
+
 import LoginForm from "../LoginForm/LoginForm";
 
 interface LoginModalProps {
@@ -11,12 +11,7 @@ interface LoginModalProps {
 
 const LoginModal = ({ className, isOpen, onClose }: LoginModalProps) => {
   return (
-    <Modal
-      className={classNames(styles.LoginModal, {}, [className])}
-      isOpen={isOpen}
-      onClose={onClose}
-      lazy
-    >
+    <Modal className={classNames("", {}, [className])} isOpen={isOpen} onClose={onClose} lazy>
       <LoginForm />
     </Modal>
   );
